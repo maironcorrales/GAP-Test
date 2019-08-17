85ab4f080ab08f27a92037cdf9d71dbe37e23290
maicorrales <maironcorrales@gmail.com>
2019-08-17 01:45:02 -0600

    project setting. Layout and ViewStart

---
+++ GAP_Test/GAP_Tec_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GAP_Tec_Test.MvcApplication" Language="C#" %>
---
+++ GAP_Test/GAP_Tec_Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GAP_Tec_Test.Startup))]
+namespace GAP_Tec_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
