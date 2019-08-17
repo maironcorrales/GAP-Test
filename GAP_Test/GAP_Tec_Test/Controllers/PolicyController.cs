df55c17e86f3240ff45d312f8b306ec6abdcbf7d
maicorrales <maironcorrales@gmail.com>
2019-08-17 14:18:27 -0600

    Create and delete
    functions to create and delete Policies have been tested and implemented

--- GAP_Test/GAP_Tec_Test/Controllers/PolicyController.cs
+++ GAP_Test/GAP_Tec_Test/Controllers/PolicyController.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
+using Model.Entities;
 
 namespace GAP_Tec_Test.Controllers
 {
     public class PolicyController : Controller
     {
+
+        UnitOfWork uw = new UnitOfWork(new GAPTestEntities());
+
         // GET: Policy
         public ActionResult Policies()
         {
+            List <Policy> policyList = uw.Policies.GetAll().ToList();
+            ViewBag.policies = policyList;
             return View();
+        }
+
+        public ActionResult Policy(Policy model)
+        {
+            
+            uw.Policies.Add(model);
+            uw.Complete();
+            return Redirect("~/Policy/Policies");
+        }
+
+        public ActionResult EditPolicy(Policy model, int id)
+        {
+            Policy policy = uw.Policies.Get(id);
+            policy.policy_name = model.policy_name;
+            policy.policy_description = model.policy_description;
+            uw.Complete();
+            return Redirect("~/Policy/Policies");
+        }
+
+        public ActionResult DeletePolicy(Policy model)
+        {
+            Policy policy = uw.Policies.Get(model.id_policy);
+            uw.Policies.Remove(policy);
+            uw.Complete();
+            return Redirect("~/Policy/Policies");
         }
     }
 }
