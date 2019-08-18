36cca009ee523e6d935308814f8062b394362113
maicorrales <maironcorrales@gmail.com>
2019-08-17 21:16:35 -0600

    plans for clients
    Editing of existing plans functionallity implemented

--- GAP_Test/GAP_Tec_Test/Controllers/ClientController.cs
+++ GAP_Test/GAP_Tec_Test/Controllers/ClientController.cs
@@ -13,6 +13,7 @@ namespace GAP_Tec_Test.Controllers
     {
         UnitOfWork uw = new UnitOfWork(new GAPTestEntities());
         Client client = new Client();
+        Client_has_Policy clientHasP = new Client_has_Policy(); 
         // GET: Client
         public ActionResult Client()
         {
@@ -47,8 +48,6 @@ namespace GAP_Tec_Test.Controllers
             ViewBag.pliciesOfClient = clientPolicyList;
             ViewBag.client = uw.Clients.Get(id);
             List<Policy> policyList = uw.Policies.GetAll().ToList();
-            List<string> risks = new List<string>{"Low", "Medium", "Mediwm-high", "High"};
-            ViewBag.risks = new SelectList(risks, "risks", "risks");
             ViewBag.policies = new SelectList(policyList,"id_policy","policy_name");
             return View();
         }
@@ -65,6 +64,10 @@ namespace GAP_Tec_Test.Controllers
         public ActionResult AddPolicy(FormCollection collection, Client_has_Policy model)
         {
             model.client_id_client = Convert.ToInt32(collection[0]);
+            if (model.risk == 4 && model.coverage_amount > 50)
+            {
+                model.coverage_amount = 50;
+            }
             uw.ClienHasPolicies.Add(model);
             uw.Complete();
             return Redirect("~/Client/AddPolicyToClient/"+model.client_id_client);
@@ -78,6 +81,29 @@ namespace GAP_Tec_Test.Controllers
             uw.ClienHasPolicies.Remove(chp);
             uw.Complete();
             return Redirect("~/Client/AddPolicyToClient/" + param);
+        }
+
+        public ActionResult EditPlan(int id)
+        {
+            List<Policy> policyList = uw.Policies.GetAll().ToList();
+            ViewBag.policies = new SelectList(policyList, "id_policy", "policy_name");
+            return View(clientHasP = uw.ClienHasPolicies.Get(id));
+        }
+
+        public ActionResult EditPolicy(Client_has_Policy model) 
+        {
+            Client_has_Policy chp = uw.ClienHasPolicies.Get(model.id_coverage_plan);
+            chp.plan_Coverage_description = model.plan_Coverage_description;
+            chp.coverage_period = model.coverage_period;
+            chp.coverage_start = model.coverage_start;
+            chp.cover_object = model.cover_object;
+            chp.coverage_amount = model.coverage_amount;
+            chp.coverage_monthly_price = model.coverage_monthly_price;
+            chp.risk = model.risk;
+            if (chp.risk == 4 && chp.coverage_amount > 50)
+                chp.coverage_amount = 50;
+            uw.Complete();
+            return Redirect("~/Client/AddPolicyToClient/" + chp.client_id_client);
         }
 
         
